32a33f90dcfabc892406392cb8e5f40c3bfdf5e5
Carson Angell <carsonjesseangell@gmail.com>
2025-10-14 19:30:44 -0600

    Added List interface stubs to SinglyLinkedList

--- app/src/main/java/assign06/SinglyLinkedList.java
+++ app/src/main/java/assign06/SinglyLinkedList.java
@@ -1,5 +1,92 @@
 package assign06;
 
-public class SinglyLinkedList {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SinglyLinkedList<E> implements List<E> {
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public E get(int index) throws IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public E getFirst() throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFirst'");
+    }
+
+    @Override
+    public E getLast() throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLast'");
+    }
+
+    @Override
+    public int indexOf(E element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'indexOf'");
+    }
+
+    @Override
+    public void insert(int index, E element) throws IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void insertFirst(E element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insertFirst'");
+    }
+
+    @Override
+    public void insertLast(E element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insertLast'");
+    }
+
+    @Override
+    public E delete(int index) throws IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public E deleteFirst() throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteFirst'");
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+    }
+
+    @Override
+    public Object[] toArray() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toArray'");
+    }
     
 }
